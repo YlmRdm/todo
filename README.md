734073b8a27943f5fc208937666df41d346ffc0e
YlmRdm <yalim.erdem@outlook.es>
2019-10-03 16:18:52 +0200

    Last commit...

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # todo
 This application is developed with Django and React…
+
+## Database: PostgreSQL
+- Version: 11.5
+- pgAdmin 4
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'todos',
         'USER': 'postgres',
-        'PASSWORD': 'mithra23',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
